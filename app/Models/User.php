426083d4cbecf1a0019468d1515a7b27c22ea217
Najie <rsyzark@gmail.com>
2022-05-20 17:48:34 +0700

    change User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function invitation()
+    {
+        return $this->hasOne(invitation::class, 'user_id');
+    }
 }
